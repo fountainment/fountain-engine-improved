9b578dcdf396bae8f18a6e946210775a17c38f19
C.Even <fountainment@gmail.com>
2015-09-24 14:39:28 +0800

    update Vec3

--- fountain/math/Vec3.h
+++ fountain/math/Vec3.h
@@ -12,11 +12,43 @@ public:
 	Vec3(float xx, float yy, float zz);
 	Vec3(const Vec2& v, float zz);
 
+	void mul(float f);
+	void div(float f);
+
+	const Vec3 operator*(float f) const;
+	const Vec3 operator/(float f) const;
+
 	float x;
 	float y;
 	float z;
 };
 
+}
+
+inline void fei::Vec3::mul(float f)
+{
+	x *= f;
+	y *= f;
+	z *= f;
+}
+
+inline void fei::Vec3::div(float f)
+{
+	mul(1.0f / f);
+}
+
+inline const fei::Vec3 fei::Vec3::operator*(float f) const
+{
+	fei::Vec3 result(*this);
+	result.mul(f);
+	return result;
+}
+
+inline const fei::Vec3 fei::Vec3::operator/(float f) const
+{
+	fei::Vec3 result(*this);
+	result.div(f);
+	return result;
 }
 
 #endif
