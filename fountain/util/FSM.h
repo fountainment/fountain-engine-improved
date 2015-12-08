7698d8f036c8aecb9f0c0e574853f5917b086aaf
非天 <fountainment@gmail.com>
2015-12-08 18:06:07 +0800

    add FSM class

---
+++ fountain/util/FSM.h
@@ -0,0 +1,46 @@
+#if !defined(_FUT_FSM_H_)
+#define _FUT_FSM_H_
+
+#include <string>
+#include <map>
+
+namespace fut {
+
+class FSM
+{
+public:
+	FSM();
+
+	void inputSignal(int signal);
+
+	int registerSignal(const std::string& signal);
+	int registerState(const std::string& state);
+	void registerLink(const std::string& curState, \
+			const std::string& nextState, const std::string& signal);
+
+	int registerSignal();
+	int registerState();
+	void registerLink(int curState, int nextState, int signal);
+
+	int getState();
+	void setState(int state);
+
+	static const int NONE_SIG = 0;
+	static const int CHANGE_SIG = 1;
+
+private:
+	virtual void outputSignal(int signal);
+
+	int _MAX_SIG = 1;
+	int _MAX_STATE = 1;
+	int _state;
+	std::map<std::string, int> _nameSignalMap;
+	std::map<int, std::string> _signalNameMap;
+	std::map<std::string, int> _nameStateMap;
+	std::map<int, std::string> _stateNameMap;
+	std::map<int, std::map<int, int>> _fsmMap;
+};
+
+} // namespace fut
+
+#endif // _FUT_FSM_H_
